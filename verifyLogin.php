18a0f999d7f02ac41e941db90550de0b401aaab7
Dalton2727 <dlsoper27@gmail.com>
2025-03-05 15:08:54 -0500

    Add files via upload
    Adds all files that allow a login. Successful login redirects user to start.html

---
+++ verifyLogin.php
@@ -0,0 +1,20 @@
+<?php
+include 'dbconnection.php';
+$userid = $_POST['userid'];
+$password = $_POST['password'];
+$sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+$stmt = mysqli_prepare($db, $sql);
+mysqli_stmt_bind_param($stmt, "ss", $userid, $password);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$num = mysqli_num_rows($result);
+
+// "start.html" is the page that a successful login that will redirect the user to
+if ($num > 0) {
+    header($url = "start.html");
+    header('Location:'. $url);
+    die();   
+} else {
+  echo "Wrong User id or password";
+}
+?>
