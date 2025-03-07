aae5a8b02039df1826af5f56c87137018d2a7f60
Dalton2727 <dlsoper27@gmail.com>
2025-03-07 01:20:32 -0500

    Update write.php

--- write.php
+++ write.php
@@ -3,15 +3,28 @@ session_start();
 include 'dbconnection.php';
 $userid = isset($_GET['userid']) ? $_GET['userid'] : '';
 ?>
-
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Review writing form</title>
-    <link rel="stylesheet" type="text/css" href="CSScode.css" />
-  </head>
-
-  <body>
+<html lang="en">
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="Swings points balance" content="" />
+        <meta name="description" content="Wesleyan University Point budgeter"/>
+        <link rel="stylesheet" href="CSScode.css" />
+    </head>
+<body id = "font" style="background-color:rgb(243, 70, 70);">
+            <div id="navbar">
+                <ul>
+                    <li> <?php echo '<a href="Edit.php?userid=' . urlencode($userid) . '">Edit review</a>'?> </li>
+                    <li> <?php echo '<a href="write.php?userid=' . urlencode($userid) . '">Write a review</a>'?> </li>
+                    <li style="color: white;">User: <?php echo $userid?></li>
+                    <li> <?php echo '<a href="start2.php?userid=' . urlencode($userid) . '">Home</a>'?> </li>
+                    <li> <?php echo '<a href="about2.php?userid=' . urlencode($userid) . '#about">About</a>'; ?> </li>
+                    <li> <?php echo '<a href="about2.php?userid=' . urlencode($userid) . '#menu">Menu</a>'; ?> </li>
+                    <li> <?php echo '<a href="about2.php?userid=' . urlencode($userid) . '#other">Other</a>'; ?> </li>
+                    <li><?php if ($_SESSION['loggedin']){ echo '<a href="reviews.php?userid=' . urlencode($userid) . '">Start</a>';} else {echo '<a href="index.php">Start</a>';}?></li>
+                </ul>
+              </div>
     <div id="form">
       <h1> Write your review for a specific meal offered at a Wesleyan dining location</h1>
       <form name="form" action="uploadreview.php" method="POST">
